0471124bc1a15e1ca83df0e5b83480df99f9e48c
yjin0034 <yjin0034@naver.com>
2022-09-21 21:52:15 +0900

    Add Search Function

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -8,23 +8,43 @@
 
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
 
 # index 페이지 관련 함수
 def index(request):
-    # 페이지  # GET 방식으로 호출된 URL에서 page 값을 가져올 때 사용
+    # 페이지
+    # GET 방식으로 호출된 URL에서 page 값을 가져올 때 사용
     # page 값 없이 호출된 경우에는 디폴트로 1이라는 값이 설정되게 함
     page = request.GET.get('page', '1')
+    # 검색어
+    kw = request.GET.get('kw', '')
     # Question의 질문 목록 데이터 얻기
     # order_by('-create_date') : 작성 일시를 역순으로 정렬 # - : 역방향
     question_list = Question.objects.order_by('-create_date')
+    # Q 함수 : OR조건으로 데이터를 조회하기 위해 사용하는 함수. 제목, 내용, 글쓴이를 OR 조건으로 검색하기 위해 사용
+    # distinct 함수 : 조회 결과에 중복이 있을 경우 중복을 제거하여 리턴하는 함수
+    # 하나의 질문에는 여러 개의 답변이 있을 수 있는데, 이 때 여러 개의 답변이 검색 조건에 해당될 때 동일한 질문이 중복으로 조회될 수 있다. 이런 이유로 중복된 질문을 제거하기 위해 distinct를 사용
+    # subject__icontains=kw : 제목에 kw 문자열이 포함되었는지를 확인
+    # answer__author__username__icontains=kw : 답변을 작성한 사람의 이름에 kw 문자열이 포함되는지 확인
+    # filter 함수에서 모델 속성에 접근하기 위해서는, 이처럼 __(언더바 두개)를 이용하여 하위 속성에 접근할 수 있다.
+    # contains과 icontains의 차이점 : icontains를 사용하면 대소문자를 가리지 않고 찾아준다.
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
     paginator = Paginator(question_list, 10)  # (질문 목록 데이터를) 페이지당 10개씩 보여주기
     # paginator를 이용하여 요청된 페이지(page)에 해당되는 페이징 객체(page_obj)를 생성
     # 이렇게 하면 장고 내부적으로는 데이터 전체를 조회하지 않고, 해당 페이지의 데이터만 조회하도록 쿼리가 변경됨
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    # context = {..., 'page': page, 'kw': kw} : page와 kw를 템플릿에 전달하기 위해 context 딕셔너리에 추가
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     # 질문 목록으로 조회한 question_list 데이터(context)를 템플릿 파일(pybo/question_list.html)에 적용하여 HTML을 생성한 후 리턴
     # render 함수 : 파이썬 데이터를 템플릿에 적용하여 HTML로 반환하는 함수
     return render(request, 'pybo/question_list.html', context)
